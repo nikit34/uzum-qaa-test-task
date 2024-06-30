71002389a8615abb5f919b1b9e1785a29671cac2
nikit34 <permikov134@mail.ru>
2024-06-30 12:03:57 +0100

    Add query to db

--- integration-tests-rest/main.go
+++ integration-tests-rest/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/gorilla/mux"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -15,6 +17,19 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 )
+
+type jsonError struct {
+	Code string `json:"code"`
+	Msg string `json:"msg"`
+}
+
+type Book struct {
+	ISBN string `json:"isbn"`
+	Title string `json:"title"`
+	Image string `json:"image"`
+	Genre string `json:"genre"`
+	YearPublished int `json:"year_published"`
+}
 
 type health struct {
 	Status string `json:"status"`
@@ -54,9 +69,46 @@ func main() {
 
 			b, _ := json.Marshal(h)
 
-			w.Write(b)
 			w.WriteHeader(http.StatusOK)
-	})
+			w.Write(b)
+		},
+	)
+	r.HandleFunc(
+		"/book/{isbn}",
+		func(w http.ResponseWriter, r *http.Request) {
+			v := mux.Vars(r)
+
+			isbn := v["isbn"]
+
+			b := Book{}
+			row := db.QueryRow("SELECT isbn, name, image, genre, year_published FROM book WHERE isbn = $1", isbn)
+			err := row.Scan(
+				&b.ISBN,
+				&b.Title,
+				&b.Image,
+				&b.Genre,
+				&b.YearPublished,
+				)
+
+			if err != nil {
+				e := jsonError{
+					Code: "001",
+					Msg:  fmt.Sprintf("No book with ISBN %s", isbn),
+				}
+
+				body, _ := json.Marshal(e)
+
+				w.WriteHeader(http.StatusNotFound)
+				w.Write(body)
+				return
+			}
+
+			body, _ := json.Marshal(b)
+
+			w.WriteHeader(http.StatusOK)
+			w.Write(body)
+		},
+	)
 
 	s := http.Server{
 		Addr:              ":8080",
